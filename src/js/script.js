d3f7fdf697945e4292a3a1ad0fb3b584fc7c7037
Taissa Rodrigues <141452801+taissarodrigues@users.noreply.github.com>
2026-03-04 10:49:18 -0300

    feat(header): implementa toggle do menu mobile

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const hamburguer = document.querySelector('#hamburguer-button');
+const navList = document.querySelector('.nav-list');
+
+hamburguer.addEventListener('click', () => {
+  hamburguer.classList.toggle('active');
+  navList.classList.toggle('active');
+});
